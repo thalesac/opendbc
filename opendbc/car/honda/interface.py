93f9673dde7623a4fbe7519f5c8ebbf1e1424011
thales ceolin <thales@ulevel.com>
2026-02-27 18:24:34 -0300

    Honda City 7G: fix low-speed brake control
    Add vEgoStopping=2.0 and stoppingDecelRate=0.3 for proper braking
    behavior when approaching stops. From mvl-boston/fit-taiwan-vibration.

--- opendbc/car/honda/interface.py
+++ opendbc/car/honda/interface.py
@@ -92,6 +92,10 @@ class CarInterface(CarInterfaceBase):
       ret.longitudinalTuning.kiBP = [0., 5., 35.]
       ret.longitudinalTuning.kiV = [1.2, 0.8, 0.5]
 
+    if candidate == CAR.HONDA_CITY_7G:
+      ret.vEgoStopping = 2.0
+      ret.stoppingDecelRate = 0.3
+
     # Disable control if EPS mod detected
     for fw in car_fw:
       if fw.ecu == "eps" and b"," in fw.fwVersion:
